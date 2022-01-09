edc97587e0f1d4146c22d4e9ea2a0c7a7688574d
Yevhen Malyshko <memx86@gmail.com>
2022-01-10 00:11:15 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+centerForm();
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const firstDelay = toNumber(e.target.delay.value);
+  const stepDelay = toNumber(e.target.step.value);
+  const amount = toNumber(e.target.amount.value);
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let delay = firstDelay + stepDelay * i;
+    createPromise(position, delay).then(onSuccess).catch(onReject);
+  }
+}
+
+function toNumber(value) {
+  const num = Number.parseInt(value);
+  if (num < 0) return 0;
+  return num;
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+function onSuccess({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+function onReject({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+}
+function centerForm() {
+  const mainRef = document.createElement('main');
+  const centerRef = document.createElement('div');
+  centerRef.classList.add('center');
+  mainRef.appendChild(centerRef);
+  refs.form.before(mainRef);
+  centerRef.appendChild(refs.form);
 }
